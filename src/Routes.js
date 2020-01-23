2cc81964cde1d5cf0efbe55c7209d40b66ac6427
Samuel Rodrigues <samuelrodrigues@MacBook-Pro-de-Samuel.local>
2020-01-23 09:19:35 -0300

    Criação do  componente Home. Aviso de not found

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 //Importando componentes 
-import App from './App'
+import Home from './pages/Home'
 import Page1 from './pages/Page1'
 import Page2 from './pages/Page2'
 
@@ -14,13 +14,14 @@ class Routes extends React.Component {
   render() {
     return (
       <Switch>
+        <Route exact path='/' component={Home} />
         <Route exact path='/page1' component={Page1} />
         <Route exact path='/page2' component={Page2} />
 
         <Route path='/page1/page1.1' component={Mini1} />
         <Route
           render={() => {
-            return <h1> Home </h1>
+            return <h1> Página não encontrada </h1>
           }}
         />
       </Switch>
